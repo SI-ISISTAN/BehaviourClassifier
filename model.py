a5415e948b925fd043089e240a0a48649239f9ad
Francisco <francisco.serrano372@gmail.com>
2018-06-13 14:57:05 -0300

    Se acomodó la representación para alimentar el clasificador

--- model.py
+++ model.py
@@ -1,21 +1,20 @@
+import preprocessing
 import preprocessingNUEVO
 
 from sklearn.naive_bayes import GaussianNB
 
-# embeddings_dict = preprocessingNUEVO.load_embeddings('datasets/SBW-vectors-300-min5.txt')
+embeddings_dict = preprocessingNUEVO.load_embeddings('datasets/SBW-vectors-300-min5.txt')
+classes, messages = preprocessingNUEVO.load_data('datasets/messages-hangouts.txt')
+tfidf = preprocessingNUEVO.obtain_tfidf_values(messages)
+embeddings = preprocessingNUEVO.obtain_embeddings_from_conversation(messages, embeddings_dict)
+sentences2vec = preprocessingNUEVO.obtain_sentences2vec(tfidf, embeddings)
 
-# TODO: solucionar la parte de que el tfidf retorne la misma cantidad
-
-classes, messages = preprocessingNUEVO.load_data('datasets/messages-hangouts-prueba.txt')
-tfidf_values, vectorizer = preprocessingNUEVO.obtain_tfidf_values(messages)
-# embeddings = preprocessingNUEVO.obtain_embeddings_from_conversation(messages, embeddings_dict)
-
-
-
+# classifier = GaussianNB()
+# classifier.fit(sentences2vec, classes)
 
 
 
-
+#
 # messages_json, classes_json = preprocessing.load_data('chats-lotr.json')
 # tfidf_values_json = preprocessing.obtain_tfidf_values(list(map(lambda msg_list: ' '.join(msg_list), messages_json)))
 # embeddings_json = preprocessing.convert_words_into_embeddings(messages_json, content, embeddings_dict)
--- preprocessingNUEVO.py
+++ preprocessingNUEVO.py
@@ -16,7 +16,7 @@ def load_embeddings(txt_dir):
 
             id_embedding += 1
             if id_embedding % 100000 == 0:
-                print(id_embedding, 'de aprox. 1000000')
+                print('Van', id_embedding, 'de aprox. 1000000')
 
     print('Se generó el índice de embeddings')
 
@@ -35,18 +35,27 @@ def load_data(file_dir):
     classes = np.array(list(map(lambda tupla: int(tupla[0]), content)))
     messages = list(map(lambda tupla: tupla[1], content))
 
+    print('Se cargó el archivo:', file_dir)
+
     return classes, messages
 
 
-def obtain_tfidf_values(document_list):
+def obtain_tfidf_values(messages):
     vectorizer = TfidfVectorizer(token_pattern=r"(?u)\b\w\w*\b")
-    sparse_matrix = vectorizer.fit_transform(document_list)
+    sparse_matrix = vectorizer.fit_transform(messages)
 
-    tfidf = {}
+    tfidf = []
     for i in range(0, sparse_matrix.shape[0]):
-        tfidf[i] = sparse_matrix[i]
+        word_list = messages[i].split()
+        word_list = list(map(lambda word: vectorizer.vocabulary_[word], word_list))
+        word_list = list(map(lambda word: sparse_matrix.indices.tolist().index(word), word_list))
+        word_list = np.array(list(map(lambda word: sparse_matrix.data[word], word_list)))
 
-    return tfidf, vectorizer
+        tfidf.append(word_list)
+
+    print('Se terminó de armar la estructura de tfidf')
+
+    return tfidf
 
 
 def obtain_embeddings_from_conversation(messages, embeddings_dict):
@@ -62,9 +71,17 @@ def obtain_embeddings_from_conversation(messages, embeddings_dict):
     return list(map(lambda msg: obtain_embeddings_from_sentence(msg), messages))
 
 
-def obtain_sentences2vec(messages, tfidf_values, embeddings):
+def obtain_sentences2vec(tfidf, embeddings):
+    # Multiplico cada embedding por su correspondiente TFIDF
+    for i in range(0, len(tfidf)):
+        for j in range(0, len(tfidf[i])):
+            embeddings[i][j] = np.multiply(embeddings[i][j], tfidf[i][j])
+
+    print('Se arranca a armar el sentences2vec')
+
+    # Para cada oración, armo el promedio de los vectores de cada palabra
     sentences2vec = []  # Sería una lista de numpy array con los vectores de cada ORACION
+    for embedding in embeddings:
+        sentences2vec.append(np.mean(np.array(embedding), axis=0))
 
-    for i in range(0, len(embeddings)):
-        tfidf = tfidf_values
-
+    return sentences2vec
